d59988f7f797b656f6e67f32bbe97ece132d740e
soncomqiq <idgamescs@hotmail.com>
2023-05-10 01:52:42 +0700

    Finish Lab-r3-l5 method 1 - ternary operator

--- src/components/StudentList/StudentList.jsx
+++ src/components/StudentList/StudentList.jsx
@@ -6,13 +6,16 @@ export function StudentList(props) {
   const studentList = props.studentList;
   const [curBType, setCurBType] = useState("A");
 
-  const filteredStudends = studentList.filter(e => e.bType === curBType);
+  const filteredStudents = studentList.filter((e) => e.bType === curBType);
 
   return (
     <>
       <div className="selectdiv">
         <label>
-          <select value={curBType} onChange={e => setCurBType(e.target.value)}>
+          <select
+            value={curBType}
+            onChange={(e) => setCurBType(e.target.value)}
+          >
             <option value="A">A</option>
             <option value="B">B</option>
             <option value="O">O</option>
@@ -21,15 +24,19 @@ export function StudentList(props) {
         </label>
       </div>
       <div>
-        {filteredStudends.map((e) => (
-          <StudentItem
-            key={e.id}
-            name={e.name}
-            surname={e.surname}
-            age={e.age}
-            bType={e.bType}
-          />
-        ))}
+        {filteredStudents.length === 0 ? (
+          <div>Not found</div>
+        ) : (
+          filteredStudents.map((e) => (
+            <StudentItem
+              key={e.id}
+              name={e.name}
+              surname={e.surname}
+              age={e.age}
+              bType={e.bType}
+            />
+          ))
+        )}
       </div>
     </>
   );
